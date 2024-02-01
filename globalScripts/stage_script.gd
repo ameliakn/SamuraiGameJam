ad45f18cbfb35c4bef3903ed46fa81d5a2fa44f1
ameliakn <ameliaknfs@gmail.com>
2024-01-31 23:07:13 -0300

    deathscreen

--- globalScripts/stage_script.gd
+++ globalScripts/stage_script.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @onready var audio_death = $audio_death
+@onready var death_screen = $death_screen
 
 @export var next_stage_file: String
 
@@ -10,10 +11,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("reset"):
+		death_screen.visible = false
 		get_tree().reload_current_scene()
 		
 func _stage_finish():
 	get_tree().change_scene_to_file(next_stage_file)
 #
 func _on_character_body_2d_player_dead():
+	death_screen.visible = true
 	audio_death.play()
